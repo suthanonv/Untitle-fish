4420ff5cde331941190d1f2c2975a242485be4cb
Suthanon <122354357+suthanonv@users.noreply.github.com>
2025-06-24 22:52:48 +0700

    [Add] OnSetUp Listener

--- Assets/Script/PhaseController/Listener/SetUpListener.cs
+++ Assets/Script/PhaseController/Listener/SetUpListener.cs
@@ -1,6 +1,20 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SetUpListener : MonoBehaviour
 {
+    [SerializeField] UnityEvent OnSetUp;
+    public event Action OnSetUpEvent;
 
+    private void Awake()
+    {
+        FindAnyObjectByType<PhaseController>().OnSetUpEvent += SetUp;
+    }
+
+    void SetUp()
+    {
+        OnSetUp?.Invoke();
+        OnSetUpEvent?.Invoke();
+    }
 }
